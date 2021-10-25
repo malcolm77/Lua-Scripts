380991793ae151ba7899864e53d0fd844a2cf3e4
Malcolm Chalmers <malcolmchalmers@hotmail.com>
2021-10-25 13:25:46 +1100

    added achievement to events list removed wait procedure

--- DingScreenshot/DingScreenshot.lua
+++ DingScreenshot/DingScreenshot.lua
@@ -1,9 +1,5 @@
 local parentframe = CreateFrame("FRAME", "dingscreenshot", UIParent)
 
-local function WaitEvent()
-	C_Timer.After(2, function() ScreenshotEvent() end)
-end
-
 function ScreenshotEvent()
 	RequestTimePlayed()
 	C_Timer.After(2, function() Screenshot() end)
@@ -12,5 +8,5 @@ end
 
 
 parentframe:RegisterEvent("PLAYER_LEVEL_UP")
---parentframe:RegisterEvent("PLAYER_ENTERING_WORLD")
+parentframe:RegisterEvent("ACHIEVEMENT_EARNED")
 parentframe:SetScript("OnEvent", ScreenshotEvent)
